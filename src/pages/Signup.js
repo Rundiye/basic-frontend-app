2a06761d96f4d664e29f9a14b3c1dcb686cef193
Jundi Ye <rundiye93@gmail.com>
2019-08-14 15:25:35 +0200

    added email section in signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -8,19 +8,22 @@ class Signup extends Component {
   state = {
     username: '',
     password: '',
+    email: ''
   };
 
   handleFormSubmit = (event) => {
     event.preventDefault();
     const username = this.state.username;
     const password = this.state.password;
+    const email = this.state.password;
 
-    auth.signup({ username, password })
+    auth.signup({ username, password, email })
       .then( (user) => {
         console.log(user)
         this.setState({
             username: '',
             password: '',
+            email: ''
         });
       })
       .catch( error => console.log(error) )
@@ -32,14 +35,16 @@ class Signup extends Component {
   }
 
   render() {
-    const { username, password } = this.state;
+    const { username, password, email } = this.state;
     return (
       <>
         <form onSubmit={this.handleFormSubmit}>
           <label htmlFor='username'>Username:</label>
-          <input id='username' type='text' name='username' value={username} onChange={this.handleChange}/>
+          <input id='username' type='text' name='username' required value={username} onChange={this.handleChange}/>
+          <label htmlFor='email'>Email:</label>
+          <input id='email' type='text' name='email' required value={email} onChange={this.handleChange}/>
           <label htmlFor='password'>Password:</label>
-          <input id='password' type='password' name='password' value={password} onChange={this.handleChange} />
+          <input id='password' type='password' name='password' required value={password} onChange={this.handleChange} />
           <input type='submit' value='Signup' />
         </form>
 
