c5126af66dfaa1145675191e7f96762087a86d09
Uros Cirkovic <cirkovic.uros.srb@gmail.com>
2019-08-21 11:20:52 +0200

    Update dashboard and activities

--- src/components/ActivityList.js
+++ src/components/ActivityList.js
@@ -1,27 +1,24 @@
-import React from 'react'
-import withAuth from '../components/withAuth'
-import Activity from './Activity'
+import React from 'react';
+import withAuth from '../components/withAuth';
+import Activity from './Activity';
 
-
-const ActivityList = (props) =>  {
-  
-
-    const {activities} = props;
-      return (
-        <div>
-          {activities ? activities.map((activity, index) => {
+const ActivityList = props => {
+  const { activities, updateDashboard } = props;
+  return (
+    <div>
+      {activities
+        ? activities.map((activity, index) => {
             return (
-              <article className="activity-card" key={index} >
-                <Activity activity={activity}/>
+              <article className="activity-card" key={index}>
+                <Activity
+                  updateDashboard={updateDashboard}
+                  activity={activity}
+                />
               </article>
-            )
-          }) : null}  
-        </div>
-        )
-}
-export default  withAuth(ActivityList);
-
-
-  
-  
-
+            );
+          })
+        : null}
+    </div>
+  );
+};
+export default withAuth(ActivityList);
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,89 +1,89 @@
-
-import React, { Component } from 'react'
-import {Link} from 'react-router-dom'
-import withAuth from '../components/withAuth'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import withAuth from '../components/withAuth';
 import Navbar from '../components/Navbar';
-import ActivityList from '../components/ActivityList'
+import ActivityList from '../components/ActivityList';
 // import Explore from '../components/Explore'
 // import Day from '../components/Day'
-import tripService from '../services/trip-service'
-import moment from 'moment'
-
+import tripService from '../services/trip-service';
+import moment from 'moment';
 
 // const arrFormats = null;
 
 class Dashboard extends Component {
-
   state = {
     startDate: '',
     endDate: '',
     budget: 0,
     totalDays: [],
     id: this.props.match.params.id,
-    isLoading: true
-  }
+    isLoading: true,
+  };
 
   componentDidMount() {
-      this.getAllTrips()
-    
+    this.getSingleTripData();
   }
 
-  getAllTrips = () => {
-    tripService.getSingleTrip(this.state.id)
-    .then((response)=> {
-      const {startDate, endDate, totalDays, budget} = response.data
-      
-      this.setState({
-        startDate,
-        endDate,
-        totalDays,
-        budget,
-        isLoading: false
-      })
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-}
+  getSingleTripData = () => {
+    tripService
+      .getSingleTrip(this.state.id)
+      .then(response => {
+        const { startDate, endDate, totalDays, budget } = response.data;
 
+        this.setState({
+          startDate,
+          endDate,
+          totalDays,
+          budget,
+          isLoading: false,
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   render() {
-    console.log("RENDERING");
-    console.log("HERE", this.state.totalDays)
+    console.log('RENDERING');
+    console.log('HERE', this.state.totalDays);
 
     return (
       <div>
         <div className="page-container">
           <h2 className="page-title">Dashboard</h2>
           {this.state.totalDays.map((day, index) => {
-              return (
-                <div key={day._id}>
-                  <section className="title-section">
-                    <div>
-                      <h2 className="title-style">
-                        {moment(day.date).format('LL')}
-                      </h2>
-                    </div>
-                    <div>
-                      <Link to={`/newactivity/${day._id}`}> 
-                        <img className="navbar-icon" src="../../images/add-activity.png" alt="icon add"/>
-                      </Link>
-                    </div>
-                  </section>
-                  <div> 
-                    <ActivityList activities={day.activities} />
+            return (
+              <div key={day._id}>
+                <section className="title-section">
+                  <div>
+                    <h2 className="title-style">
+                      {moment(day.date).format('LL')}
+                    </h2>
                   </div>
+                  <div>
+                    <Link to={`/newactivity/${day._id}`}>
+                      <img
+                        className="navbar-icon"
+                        src="../../images/add-activity.png"
+                        alt="icon add"
+                      />
+                    </Link>
+                  </div>
+                </section>
+                <div>
+                  <ActivityList
+                    updateDashboard={this.getSingleTripData}
+                    activities={day.activities}
+                  />
                 </div>
-              )
-          })
-        }
+              </div>
+            );
+          })}
         </div>
-        <Navbar /> 
+        <Navbar />
       </div>
-    )
+    );
   }
 }
-      
-
 
 export default withAuth(Dashboard);
